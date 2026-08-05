f12300bb6343bd869044d7f68af27f7ba76e15f7
Leonzmy <leon.zmelty@gmx.de>
2026-08-05 12:02:15 +0200

    Update projekte.js

--- projekte.js
+++ projekte.js
@@ -27,7 +27,7 @@
     });
   }, {
     root: null,
-    rootMargin: '0px 0px -35% 0px',
+    rootMargin: '0px 0px -20% 0px',
     threshold: 0
   });
   restTiles.forEach(el => observer.observe(el));
